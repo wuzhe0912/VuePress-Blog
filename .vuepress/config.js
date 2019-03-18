856f7f4702e300d53e4c199841a18cf9282f9afa
zhethon <kgb00128@gmail.com>
2019-03-18 08:35:40 +0800

    initial

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'PittWu',
+  description: '閱讀、寫作、Coding',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About me', link: '/about/' },
+      { text: 'Tags', link: '/tags/' }
+    ],
+    sidebar: auto
+  }
+}
